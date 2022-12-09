eaf970e243632f302fc5428a5c1bb458843d1916
giocoal <giorgiocoal@gmail.com>
2022-12-09 06:44:24 +0100

    TsCV on Random Forest Models

--- Analisi predittive/My-TSCrossValidation-Functions.R
+++ Analisi predittive/My-TSCrossValidation-Functions.R
@@ -119,3 +119,115 @@ tsCV_UCM <- function(my_xts,  forecastfunction, h = 1, window = NULL,
         return(e)
     }   
 }
+
+tsCV_RandomForest <- function(my_xts, xreg = NULL,  forecastfunction, h = 1, window = NULL, 
+    initial = 0, ...) {
+    
+    #print("ok") # CHECK
+    
+    n <- nrow(my_xts) # Lunghezza totale serie storica
+
+    #Creo una matrice xts degli errori, con lo stesso numero di righe e indice della serie storica
+    #ma con un numero di colonne h e vuota
+    e <- xts(matrix(NA, nrow = nrow(my_xts), ncol = h), order.by = index(my_xts))
+    if (initial >= n) 
+        stop("initial period too long")
+    
+    # Aggiunge h righe di NA alla matrice di regressiri (quando presenti) e y, con lo scopo di avere h valori NA
+    # Dopo la fine della serie storica, in modo da poter passare sempre h dati di test anche quando
+    # La serie storica è finita
+    if (!is.null(xreg)) {
+        if (nrow(xreg) != nrow(my_xts)) 
+            stop("xreg must be of the same size as y")
+        
+        # Creo una matrixe xts di NA, che parte dall'ultimo giorno + 1 della nostra serie storica
+        na_df <- seq(from = end(my_xts) + 1, 
+            to = end(my_xts) + h, 
+            by = "day")
+        # Stessa cosa per i regressori
+        na_df_xreg <- seq(from = end(xreg) + 1, 
+            to = end(xreg) + h, 
+            by = "day")
+
+        # Aggiungo la matrice dei NA alla fine della serie storica e della matrice dei regressori
+        my_xts <- merge(my_xts, na_df)
+        xreg <- merge(xreg, na_df_xreg)
+
+        if (nrow(xreg) != nrow(my_xts)) 
+            stop("xreg must be of the same size as y")
+
+    } else {
+        # Creo una matrixe xts di NA, che parte dall'ultimo giorno + 1 della nostra serie storica
+        na_df <- seq(from = end(my_xts) + 1, 
+            to = end(my_xts) + h, 
+            by = "day")
+
+        # Aggiungo la matrice dei NA alla fine della serie storica
+        my_xts <- merge(my_xts, na_df)
+    }
+    
+
+    #print("ok1") # CHECK
+
+    if (is.null(window)) 
+        indx <- seq(1 + initial, n - 1L)
+    else indx <- seq(window + initial, n - 1L, by = 1L)
+
+    #print("ok2") # CHECK
+
+    # Iterazioni
+    for (i in indx) {
+        # print(i) # CHECK
+        
+        # Definizione stard e end di train
+        start <- ifelse(is.null(window), 1L, ifelse(i - window >= 0L, i - window + 1L, stop("small window")))
+        end <- i  # imposto come end i + h perchè così ho il train
+        
+        # Estraggo la sotto serie storica di train e di test
+        y_subset <- my_xts[start:end,]
+        y_test <- my_xts[(end + 1):(end + h),]
+
+        # Costruzione della matrice dei ritardi
+        #### NOTA BENE: a differenza di quanto accade nel codice di gian, qui converto y in xts.
+        #### do lo stesso indice di y_subset privato delle prime 14 osservazioni (così come la matrice dei ritardi)
+        y_subset <- xts(embed(y_subset, 15), order.by = index(tail(y_subset, n = -14)))
+        colnames(y_subset) <- c("y", paste0("y_", 1:14))
+
+        if (is.null(xreg)) {
+            Y <- y_subset
+            fc <- try(suppressWarnings(forecastfunction(Y = Y, 
+                h = h)), silent = TRUE)
+        } else {
+            # Subset del train e del test dei regressori
+            xreg_subset <- xreg[start:end,]
+            xreg_future <- xreg[(end + 1):(end + h),]
+
+            ## Combino i regressori di train con la matrice dei ritardi e y
+            # Rimuovo le prime 14 osservazioni di xreg
+            xreg_subset <- tail(xreg_subset, n = -14)
+            # Combino
+            Y <- cbind(y_subset, xreg_subset)
+            names(Y) <- c(names(y_subset), names(xreg_subset))
+
+            fc <- try(suppressWarnings(forecastfunction(Y = Y,
+                newxreg = xreg_future, h = h)), silent = TRUE)
+        }
+
+        # Conversione del vettore delle predizioni in xts con le date del test set
+        fc <- as.xts(fc, order.by = date(y_test))
+        
+        # Calcolo degli errori
+        ## La posizione è i + 14 perchè i prim i 14 dati vanno persi a causa dei ritardi
+        if (!is.element("try-error", class(fc))) {
+            e[i, ] <- y_test[seq(h)] - fc[seq(h)]
+        }
+    }
+    if (h == 1) {
+        return(e[, 1L])
+    }
+    else {
+        colnames(e) <- paste("h=", 1:h, sep = "")
+        return(e)
+    }   
+}
+
